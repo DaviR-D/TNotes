0b35ba5cb687b3faa29c83cd74103702275b3cae
Davi-RD <davirdavanzo@gmail.com>
2024-04-23 11:46:12 -0300

    Início da implementação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: white;
+`;
+
+const Wrapper = styled.section`
+  padding: 0.1em;
+  background: rgb(40, 0, 90);
+`;
+
+const Input = styled.input`
+  border: none;
+  border-radius: 3px;
+  padding: 0.5em;
+  margin: 0.5em;
+  outline: none;
+  transition: outline-color 0.2s;
+  &:focus {
+    outline: 2px solid rgb(40, 0, 90);
+  }
+`;
+
+const TList = styled.li`
+  color: white;
+  background: rgb(40, 0, 90);
+  width: 25%;
+  height: 25%;
+  text-align: center;
+  padding: 0.5em;
+  border-radius: 3px;
+`;
+
+const tasks = [<TList>Tarefa 1</TList>, <TList>Tarefa 2</TList>];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Title>TNotes</Title>
+      </Wrapper>
+      <Input></Input>
+      <ul>{tasks}</ul>
     </div>
   );
 }
